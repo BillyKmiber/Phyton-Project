38435ffd15a670c23c25b657df3be5af4f3531fd
BillyKmiber <billykimber888@gmail.com>
2025-02-18 18:05:32 +0530

    dghf

---
+++ pyton.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__jerlin__)
+
+@app.route('/')
+def home():
+    return "Hello, jerlin! This is my first pyton file."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
